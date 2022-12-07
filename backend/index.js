c2815cd43110754036c35f4515d6489e0f479580
Sdfeagt <rafalciechanski15@gmail.com>
2022-12-07 18:27:19 +0200

    The program now records the time when drone enter the NDZ

--- backend/index.js
+++ backend/index.js
@@ -2,46 +2,90 @@ const xml2js = require('xml-js')
 
 const port = 3001;
 
-
 const express = require('express');
-const { raw } = require('express');
 const app = express()
 
 app.use(express.static('build'))
 
 let allDrones = []
+let allDronesDict = new Map()
+let combinedInfo = new Map()
 
 //make a function to calculate the distance between the nest and a NDZ drone
-const calculateDisct = (drone) =>{
-  const toRet = Math.sqrt((drone.positionX-250000) ** 2 + (drone.positionY-250000) ** 2)
+const calculateDist = (drone) =>{
+  const toRet = Math.sqrt((+drone.positionX._text-250000) ** 2 + (+drone.positionY._text-250000) ** 2)
   return toRet
 }
 
 const getNDZviolations = async() =>{
+  //time is declared here, as it needs to be updated
+  let date = new Date
+  let hour = date.getHours()
+  let minutes = date.getMinutes()
+  let seconds = date.getSeconds()
+  let currentTime = {hour, minutes, seconds}
     //get the drone data and change to to JSON format
     const xml = await fetch(
         "https://assignments.reaktor.com/birdnest/drones"
       ).then((res) => res.text())
-      .catch(function(error){console.log("Error in fetch: ", error);});
-      
+      .catch((error) => {
+        console.error('Error:', error)
+      })
       const result = xml2js.xml2js(xml, { compact: true });
       const clearData = result.report.capture.drone;
       allDrones = clearData
 
-      //filter the drones that break the NDZ zone
-      const dronesNDZ = allDrones.filter(drone => calculateDisct(drone)<100)
+      allDrones.forEach((drone)=>{
+        allDronesDict.set(drone.serialNumber._text, drone)
+      })
 
-      //TODO: fetch the pilot data, and get only the ones that match with drone serial number.
-      //After that, figure out a way to store all of that for 10 minutes.
+      //filter the drones that break the NDZ zone
+      const dronesNDZ = allDrones.filter(drone => calculateDist(drone)<100000)
+
+      const pilotsNDZ = await Promise.all(
+        dronesNDZ.map(async (drone) =>{
+          const pilotData = (
+            await fetch(
+              `https://assignments.reaktor.com/birdnest/pilots/${drone.serialNumber._text}`
+            ).then(result => result.json())
+        )
+        return{
+          ...pilotData,
+          distance: calculateDist(drone),
+          timeOfRecord: currentTime
+        }
+      })
+      )
+
+      //In the current implementation, the recorded time is the last time the drone was in the NDZ.
+      //AKA, the time recorded is the time the drone has left the NDZ
+      //We don't need to worry about whether the pilot in question is in the zone, as the drones are alredy filtered
+      pilotsNDZ.forEach((pilot)=>{
+        if (combinedInfo.has(pilot.pilotId)){ // update the object if it was previously recorded
+          console.log("Need to update");
+          combinedInfo.get(pilot.pilotId).distance = pilot.distance
+          combinedInfo.get(pilot.pilotId).timeOfRecord = currentTime
+
+        }
+        else{
+          combinedInfo.set(pilot.pilotId, pilot) // setup the new object
+        }
+      })
+
+      //Here ensure that if the pilot information was recorded 10 minutes ago, then delete him
 }
 
-setInterval(getRawDroneData, 2000)
+setInterval(getNDZviolations, 2000) // update the state every 2 seconds
 
 app.get('/dronedata', (request, response) => {
-    allDrones = getNDZviolations()
-    console.log(allDrones);
+  //retrieve data about all the drones
     response.end(JSON.stringify(allDrones))
   })
+
+app.get('/ndzviolations', (request, response) =>{
+  //retrieve data about the current violators
+  response.end(JSON.stringify([...combinedInfo.values()]))
+})
   
  app.listen(port, () => {
     console.log(`Server running on port ${port}`)
