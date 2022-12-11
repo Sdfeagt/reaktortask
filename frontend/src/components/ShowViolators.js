d2626b249a8e2e23c904685aa698cd20ca01de33
Sdfeagt <rafalciechanski15@gmail.com>
2022-12-11 13:00:11 +0200

    Added a cool geese pic

--- frontend/src/components/ShowViolators.js
+++ frontend/src/components/ShowViolators.js
@@ -41,7 +41,11 @@ const showViolators = ({violators}) =>{
   }
   else{
     return(
+      <div>
       <h4>If you see that, the server needs to populate with data. Let it run for a few minutes please.</h4>
+      <br/>
+      <img src="geese404.png" alt="Geese404" width="300" height="300"/>
+      </div>
     )
   }
 }
